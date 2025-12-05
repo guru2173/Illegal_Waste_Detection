023ada65bcde40dc8cad2810e35601c60588f50b
K Guru Prasad <kasiguruprasad27@gmail.com>
2025-12-05 18:51:51 +0530

    Change cv2 import to use alias cv

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import numpy as np
-import cv2
+import cv2 as cv
 import time
 
 st.set_page_config(page_title="Illegal Waste Detection")
