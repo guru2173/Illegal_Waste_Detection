fe26c52a24a5fa371edca76d045acd9637618e10
K Guru Prasad <kasiguruprasad27@gmail.com>
2025-12-05 19:26:11 +0530

    Delete fake OpenCV implementation
    Remove fake OpenCV functions and constants.

--- cv2.py
+++ cv2.py
@@ -1,16 +1,1 @@
-# FAKE OPENCV FOR STREAMLIT (NO LIBGL NEEDED)
-# Prevents Ultralytics from crashing due to missing OpenCV dependencies
 
-def imread(*args, **kwargs):
-    return None
-
-def imwrite(*args, **kwargs):
-    return None
-
-def imshow(*args, **kwargs):
-    return None
-
-def waitKey(*args, **kwargs):
-    return None
-
-COLOR_BGR2RGB = None
